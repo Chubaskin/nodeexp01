e33dadcdf9b6891edbaf24246acaa67c60a849a4
Mauricio Latorre <rhok@chubasco.org>
2017-04-29 16:15:26 -0300

    Con Admin page

--- controllers/main.js
+++ controllers/main.js
@@ -4,3 +4,8 @@ exports.init = (req, resp) => {
   // resp.send('Hola mundo desde rutas y controladores').end();
   resp.render('index')
 }
+
+exports.admin = (req, resp) => {
+  // resp.send('Hola mundo desde rutas y controladores').end();
+  resp.render('admin')
+}
--- routes/rutas.js
+++ routes/rutas.js
@@ -3,5 +3,6 @@ const Router = express.Router();
 const main = require('../controllers/main')
 
 Router.get('/', main.init);
+Router.get('/admin', main.admin);
 
 module.exports = Router;
